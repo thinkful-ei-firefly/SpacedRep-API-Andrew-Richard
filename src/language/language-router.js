2bec0ceaad4ec8e64a3d49dff0ff50f119b5a9ce
rjgates3 <gates.j.richard@gmail.com>
2019-10-16 14:32:12 -0600

    added linked list and /guess endpoint

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const LanguageService = require('./language-service')
+const LanguageHelper = require('./language-helper')
 const { requireAuth } = require('../middleware/jwt-auth')
 
 const languageRouter = express.Router()
@@ -47,33 +48,24 @@ languageRouter
   .get('/head', async (req, res, next) => {
     // get next word from the database
     try {
-      const word = await LanguageService.getNextWord(
+      const { word } = await LanguageService.getNextWord(
           req.app.get('db')
       )
-      const scores = await LanguageService.getTotalScore(
+
+      const allWords = await LanguageService.getLanguageWords(
         req.app.get('db'),
         req.language.id
       )
 
-      const totalScore = scores
-        .reduce((acc, score) => {
-          return acc + score.correct_count
-      }, 0)
+      const totalScore = LanguageService.getTotalScore(allWords)
 
       res.json({
-        "nextWord": word[0].original,
-        "wordCorrectCount": word[0].correct_count,
-        "wordIncorrectCount": word[0].incorrect_count,
+        "nextWord": word.original,
+        "wordCorrectCount": word.correct_count,
+        "wordIncorrectCount": word.incorrect_count,
         "totalScore": totalScore
       })
-      /* /head expected return
-          {
-          "nextWord": "Testnextword",
-          "wordCorrectCount": 222,
-          "wordIncorrectCount": 333,
-          "totalScore": 999
-          }
-      */
+
     } catch (error) {
       next(error)
     }
@@ -81,8 +73,100 @@ languageRouter
 
 languageRouter
   .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
+
+    try {
+      // validate req body
+      const { currWord, guess } = req.body;
+
+      if(!currWord) {
+        return res
+          .status(400)
+          .send('A "currWord" is required.')
+      }
+      if(!guess) {
+        return res
+          .status(400)
+          .send('A "guess" is required.')
+      }
+      if(typeof currWord !== 'string') {
+        return res
+          .status(400)
+          .send('"currWord" must be of type string.')
+      }
+      if(typeof guess !== 'string') {
+        return res
+          .status(400)
+          .send('"guess" must be of type string.')
+      }
+      // create linked list
+      const wordsArr = await LanguageService.getLanguageWords(
+        req.app.get('db'),
+        req.language
+      )
+      const wordsList = LanguageService.populateLinkedList(wordsArr);
+      
+      // take the word out of the linked list to edit and reinsert
+      const word = wordsList.find(currWord);
+      wordsList.remove(word)
+      
+      // check if guess is correct translation
+      const answer = word.translation
+      if(answer === guess) {
+        isCorrect = true;
+      }
+      else {
+        isCorrect = false;
+      }
+      /* 
+      set new memory_value (m)
+      if correct answer double m else m = 1 and
+       update correct and incorrect
+      */
+      if(isCorrect) {
+        word.memory_value *= 2
+        word.correct_count += 1
+      }
+      else {
+        word.memory_value = 1
+        word.incorrect_count += 1
+      }
+
+      // update totalScore
+      const allWords = await LanguageService.getLanguageWords(
+        req.app.get('db'),
+        req.language.id
+      )
+
+      const totalScore = LanguageService.getTotalScore(allWords)
+
+      // move question back m places in the list
+      const length = LanguageService.getLength(wordsList)
+      const index = (word.memory_value > length)
+        ? length
+        : word.memory_value
+      wordsList.insertAt(word, index); // length of wordList is +1
+
+      // persist the updated words and lang to database
+      LanguageHelper.persistDb(
+          req.app.get('db'),
+          wordsList,
+          length + 1
+      );
+      
+      // return json
+      res.json({
+        answer,
+        isCorrect,
+        nexWord,
+        totalScore,
+        wordCorrectCount: word.correct_count,
+        wordIncorrectCount: word.incorrect_count
+      })
+
+    }
+    catch (error) {
+      next(error)
+    }
   })
 
 module.exports = languageRouter
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const LinkedList = require('./linkedList');
+
 const LanguageService = {
     getUsersLanguage(db, user_id) {
         return db
@@ -35,17 +37,33 @@ const LanguageService = {
             .select('*')
             .limit(1);
     },
-    getTotalScore(db, language_id) {
+    update(db, id, fields) {
         return db
-            .from('word')
-            .select(
-                'language_id',
-                'correct_count'
-            )
-            .where({ language_id });
-    }
+            .where({ id })
+            .update(fields)
+    },
+    getTotalScore(allWords) {
+        allWords
+        .reduce((acc, score) => {
+          return acc + score.correct_count
+      }, 0)
+    },
+    populateLinkedList(wordsArr) {
+        let wordsList = new LinkedList();
+        return wordsArr.forEach(word => 
+            wordsList.insertLast(word)
+        );
+    },
+    getLength = (list) => {
+        let counter = 0;
+        let tempNode=list.head;
+        while (tempNode !== null) {
+            counter++;
+            tempNode=tempNode.next;
+        }
+        return counter;
+    };
 };
     
 module.exports = LanguageService;
     
-//SELECT sum(correct_count) from word where language_id = ?
